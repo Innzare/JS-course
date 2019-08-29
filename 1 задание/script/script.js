ef07b9aebe406435f24a08bccc0b933587f1fc66
Shamil <maashevshamil@gmail.com>
2019-08-29 22:35:03 +0500

    finish first proj

--- 1 задание/script/script.js
+++ 1 задание/script/script.js
@@ -1,11 +1,14 @@
 let money = prompt('Ваш бюджет на месяц?','');
 let time = prompt('"Введите дату в формате YYYY-MM-DD','');
 
+let ask1 = prompt('Введите обязательную статью расходов в этом месяце','');
+let ask2 = prompt('Во сколько обойдется?','');
+
 let appData = {
     money,
     timeData: time,
     expenses: {
-
+        ask1: ask2,
     },
     optionalExpenses: {
 
